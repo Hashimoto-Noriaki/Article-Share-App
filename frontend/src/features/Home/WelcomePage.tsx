b2b8a9a4301590b9d4a7a1e1ffc43f7e97d0d539
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-08 01:34:50 +0900

    WelcomePageのヘッターを作成

---
+++ frontend/src/features/Home/WelcomePage.tsx
@@ -0,0 +1,23 @@
+
+
+export const WelcomePage = () => {
+    return (
+        <div>
+            <header className="bg-cyan-400 p-5">
+                <div className="flex justify-between ">
+                    <div>
+                        <h1 className="text-yellow-400 font-bold text-3xl mb-3">テックブログ共有アプリ</h1>
+                        <p className="text-white font-bold">×エンジニア同士で記事を共有しよう</p>
+                    </div>
+                    <nav>
+                        <ul className="flex gap-5 text-white font-bold my-5 ">
+                            <li className="hover:text-yellow-400">利用説明</li>
+                            <li className="hover:text-yellow-400">新規ユーザー登録</li>
+                            <li className="hover:text-yellow-400">ログイン</li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </div>
+    )
+}
