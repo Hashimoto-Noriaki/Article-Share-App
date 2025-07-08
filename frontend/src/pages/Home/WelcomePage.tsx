4cf320f9682641608b0a57b3ffd6a370cd1d2c2d
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-08 22:23:24 +0900

    デザインを整える

--- frontend/src/pages/Home/WelcomePage.tsx
+++ frontend/src/pages/Home/WelcomePage.tsx
@@ -1,4 +1,4 @@
-
+import { FaLaptopCode } from 'react-icons/fa';
 
 export const WelcomePage = () => {
     return (
@@ -7,10 +7,10 @@ export const WelcomePage = () => {
                 <div className="flex justify-between">
                     <div> 
                         <h1 className="text-yellow-400 font-bold text-3xl mb-3">テックブログ共有アプリ</h1>
-                        <p className="text-white font-bold">×エンジニア同士で記事を共有しよう</p>
+                        <p className="text-white text-2xl">×エンジニア同士で記事を共有しよう</p>
                     </div>
                     <nav>
-                        <ul className="flex gap-5 text-white font-bold my-5 ">
+                        <ul className="flex gap-10 text-white font-bold p-5 text-lg">
                             <li className="hover:text-yellow-400">利用説明</li>
                             <li className="hover:text-yellow-400">新規ユーザー登録</li>
                             <li className="hover:text-yellow-400">ログイン</li>
@@ -18,15 +18,17 @@ export const WelcomePage = () => {
                     </nav>
                 </div>
             </header>
-            <main className="flex-grow flex flex-col items-center justify-center p-8">
-                <div className="bg-cyan-400 text-white px-16 py-24 rounded-md w-full max-w-5xl text-center">
-                    <h1 className="text-5xl font-bold mb-10">テックブログ共有アプリ</h1>
-                    <p className="text-3xl">×エンジニア同士で記事を共有しよう</p>
+            <main className="flex-grow flex flex-col items-center justify-center">
+                <div className="bg-gradient-to-r from-cyan-300 to-cyan-600 text-white px-16 py-24 rounded-md w-full max-w-5xl text-center shadow-lg">
+                    <h1 className="text-5xl font-bold mb-10 flex items-center justify-center">
+                        <FaLaptopCode className="text-5xl"/>テックブログ共有アプリ
+                    </h1>
+                    <p className="text-3xl">×エンジニア同士で有益な記事を共有しよう</p>
                 </div>
-                <p className="text-center text-2xl mt-10">エンジニア同士で有益な記事を共有しよう</p>
+                <p className="text-center text-2xl p-10">お気に入りの技術記事があれば投稿しよう🎵</p>
             </main>
-            <footer  className="mt-10 font-bold bg-cyan-400 text-white w-full p-7 text-center">
-                <p>© 2025 テックブログ共有アプリ</p>
+            <footer  className="mt-5 bg-cyan-400 text-white w-full p-7 text-center">
+                <p className="text-sm font-bold">© 2025 テックブログ共有アプリ</p>
             </footer>
         </div>
     )
