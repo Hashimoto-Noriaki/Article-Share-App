dd168ddbdefe327b6dda3369e731a1578164c8e3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-08 21:45:29 +0900

    WelcomePage完成

--- frontend/src/pages/Home/WelcomePage.tsx
+++ frontend/src/pages/Home/WelcomePage.tsx
@@ -2,10 +2,10 @@
 
 export const WelcomePage = () => {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <header className="bg-cyan-400 p-5">
-                <div className="flex justify-between ">
-                    <div>
+                <div className="flex justify-between">
+                    <div> 
                         <h1 className="text-yellow-400 font-bold text-3xl mb-3">テックブログ共有アプリ</h1>
                         <p className="text-white font-bold">×エンジニア同士で記事を共有しよう</p>
                     </div>
@@ -18,6 +18,16 @@ export const WelcomePage = () => {
                     </nav>
                 </div>
             </header>
+            <main className="flex-grow flex flex-col items-center justify-center p-8">
+                <div className="bg-cyan-400 text-white px-16 py-24 rounded-md w-full max-w-5xl text-center">
+                    <h1 className="text-5xl font-bold mb-10">テックブログ共有アプリ</h1>
+                    <p className="text-3xl">×エンジニア同士で記事を共有しよう</p>
+                </div>
+                <p className="text-center text-2xl mt-10">エンジニア同士で有益な記事を共有しよう</p>
+            </main>
+            <footer  className="mt-10 bg-cyan-400 text-white w-full p-10 text-center">
+                <p>© 2025 テックブログ共有アプリ</p>
+            </footer>
         </div>
     )
 }
