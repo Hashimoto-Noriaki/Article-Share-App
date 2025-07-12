caca9c4a21fc5ff63e2e0e242408a1556ce9ed2e
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-13 01:09:08 +0900

    NotLoginLayoutを定義

--- frontend/src/features/components/templates/NotLoginLayout.tsx
+++ frontend/src/features/components/templates/NotLoginLayout.tsx
@@ -1,7 +1,38 @@
-export const NotLoginLayout = () => {
-    return (
-        <div>NotLoginLayout</div>
-    )
+import { Link } from 'react-router-dom';
+import { Footer } from '../../../shared/components/atoms/Footer/Footer';
+
+type PropsType = {
+    children: React.ReactNode;
 }
 
-export default NotLoginLayout
+export const NotLoginLayout = ({children}: PropsType) => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <header className="bg-gradient-to-r from-cyan-300 to-cyan-600 p-5">
+                <div className="flex justify-between">
+                    <div> 
+                        <h1 className="text-yellow-400 font-bold text-3xl mb-3">テックブログ共有アプリ</h1>
+                        <p className="text-white text-2xl">×エンジニア同士で記事を共有しよう</p>
+                    </div>
+                    <nav>
+                        <ul className="flex gap-10 text-white font-bold p-5 text-lg">
+                            <li className="hover:text-yellow-400">
+                                <Link to='/guide'>利用説明</Link>
+                            </li>
+                            <li className="hover:text-yellow-400">
+                                <Link to='/register'>新規ユーザー登録</Link>
+                            </li>
+                            <li className="hover:text-yellow-400">
+                                <Link to='/login'>ログイン</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <main className="flex-grow flex flex-col items-center justify-center">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
+}
