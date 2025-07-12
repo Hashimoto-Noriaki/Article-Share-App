f7d4d2ce45810406b03c16690fe51d03ea347abf
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-12 13:45:58 +0900

    UserGuideのページ作成

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 import { WelcomePage } from './pages/Home/WelcomePage';
 import { LoginPage } from './pages/Login/LoginPage'; 
 import { RegisterPage } from './pages/Register/RegisterPage';
+import { UserGuidePage } from './pages/Guide/UserGuidePage';
 
 export const router = createBrowserRouter([
     {path:'/',element: <WelcomePage/>},
     {path:'/login',element: <LoginPage/>},
-    {path:'/register',element: <RegisterPage/>}
+    {path:'/register',element: <RegisterPage/>},
+    {path:'/guide',element: <UserGuidePage/>}
 ])
