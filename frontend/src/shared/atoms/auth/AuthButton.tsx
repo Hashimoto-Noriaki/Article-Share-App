f89c9d1fd70382968545dcb56141b520145bc9b5
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-12 15:15:02 +0900

    認証ボタンの修正

--- frontend/src/shared/atoms/auth/AuthButton.tsx
+++ frontend/src/shared/atoms/auth/AuthButton.tsx
@@ -4,7 +4,7 @@ type PropsType = {
 
 export const AuthButton = ({children}: PropsType) => {
     return (
-        <button className="w-full bg-emerald-600 hover:shadow-lg text-white rounded-lg font-bold py-3 mt-5 mrounded-full shadow hover:bg-emerald-500 transition">
+        <button className="w-full bg-emerald-600 hover:shadow-lg text-white rounded-lg font-bold py-3 mt-5  shadow hover:bg-emerald-500 transition">
             {children}
         </button>
     )
