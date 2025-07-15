b75d34cd6e30304456e32aa580fe0815b68adcb6
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-15 21:21:19 +0900

    React-Hook-Formを使って認証機能の実装(zodなどでバリデーションの設定)

--- frontend/src/pages/Auth/Login/LoginPage.tsx
+++ frontend/src/pages/Auth/Login/LoginPage.tsx
@@ -1,9 +1,24 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema, LoginFormValues } from "../schemas/loginSchema";
 import { Link } from 'react-router-dom';
+import { login } from '../../../api/login';
 import { FaLaptopCode } from 'react-icons/fa';
 import { AuthButton } from '../../../shared/components/atoms/auth/AuthButton';
 import { Input } from '../../../shared/components/atoms/Input/Input';
 
 export const LoginPage = () => {
+    const { register, handleSubmit,formState: { errors } } = useForm<LoginFormValues>({
+        resolver: zodResolver(loginSchema),
+    });
+
+    const onSubmit = async (data: LoginFormValues)=> {
+        try {
+            await login(data);
+        } catch {
+            console.error("ログインに失敗しました。");
+        }
+    }
 
     return (
         <div className="flex items-center justify-center max-h-screen bg-white p-10">
@@ -11,33 +26,35 @@ export const LoginPage = () => {
                 <h1 className="text-2xl flex items-center justify-center mb-5">
                     <FaLaptopCode /> テックブログ共有アプリ
                 </h1>
+                {errors.email && (
+                    <p className="text-red-500 font-bold mt-1">⚠ メールアドレスかパスワードを間違えています</p>
+                )}
                 <h2 className="text-xl font-bold mb-6">ログイン</h2>
-                <form className="flex flex-col gap-6 text-left">
+                <form className="flex flex-col gap-6 text-left" onSubmit={handleSubmit(onSubmit)}>
                     <div>
                         <p className="font-bold mb-3">メールアドレス</p>
                         <Input
                             type="email"
-                            placeholder="メールアドレス*"
+                            {...register("email")}
+                            placeholder="email"
                         />
                     </div>
                     <div>
                         <p className="font-bold mb-3">パスワード</p>
                         <Input
                             type="password"
-                            placeholder="********"
+                            {...register("password")}
+                            placeholder="password"
                         />
                     </div>
-                    <form autoComplete="off">
-                        <AuthButton onClick={() => null}>
-                        ログイン
-                        </AuthButton>
-                    </form>
+                    <AuthButton onClick={() => null}>
+                    ログイン
+                    </AuthButton>
                     <p className="mt-3 text-center text-xl underline hover:text-cyan-800">
-                        <Link to ='/register'>新規登録はこちら</Link>
+                    <Link to ='/register'>新規登録はこちら</Link>
                     </p>
                 </form>
             </div>
         </div>
-
     )
 }
