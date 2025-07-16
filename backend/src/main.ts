c659e10b9af65603ef70bdb1fa9a89d8b6165398
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-16 22:46:23 +0900

    main.tsにフロントエンド、スマホアプリからAPIが叩けるように設定

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS を許可（React、React NativeからAPI叩けるように）
+  app.enableCors({
+    origin: '*', // もしくは ['http://localhost:3000'] など
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
