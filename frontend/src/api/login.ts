706d8e7360cc5dc391ae4e79035c908f24961440
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-19 23:29:05 +0900

    ダミーAPIの修正

--- frontend/src/api/login.ts
+++ frontend/src/api/login.ts
@@ -1,14 +1,14 @@
-import { LoginInfoType, LoginReturnType } from '../types/login';
+import { LoginFormValues } from "../pages/Auth/schemas/loginSchema";
 
-export const login = async (info: LoginInfoType): Promise<LoginReturnType> => {
+export const login = async (info: LoginFormValues) => {
     const { email, password } = info;
-        if (email === "test@example.com" && password === "password123") {
-            return {
-                id: 1,
-                name: "テストユーザー",
-                email:"test@example.com",
-            };
-        } else {
-            throw new Error();
+    if (email === "test@example.com" && password === "password123") {
+        return {
+            id: 1,
+            name: "テストユーザー",
+            email: "test@example.com",
+        };
+    } else {
+        throw new Error("");
     }
 };
