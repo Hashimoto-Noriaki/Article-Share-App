9744b341fb5ef0f8f50988964d83b79b9f3406ed
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-08-05 00:18:26 +0900

    jest.setup.tsの修正

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,6 +1,6 @@
 import { TextEncoder, TextDecoder } from 'util';
 
-require('@testing-library/jest-dom');
+import('@testing-library/jest-dom');
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
