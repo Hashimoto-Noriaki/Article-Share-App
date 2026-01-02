51da6a28674000d9199ba0bfe3d81d6823be94b3
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-01-03 00:03:07 +0900

    基本モジュール構成

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,8 +7,8 @@ async function bootstrap() {
 
   // CORS を許可（React、React NativeからAPI叩けるように）
   app.enableCors({
-    origin: '*', // もしくは ['http://localhost:3000'] など
+    origin: '*',
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
